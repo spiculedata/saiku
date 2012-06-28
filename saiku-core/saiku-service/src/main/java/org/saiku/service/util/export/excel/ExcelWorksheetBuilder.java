d6763a4f37df8a4a0ba660ffc1ab510304108cc6
sramazzina <sergio.ramazzina@serasoft.it>
2012-06-29 00:27:17 +0200

    Improved Excel Export: lock table headers for better readability, expand columns to max size and added a summary page with evidence of filter eventually applied to obtain our view

--- saiku-core/saiku-service/src/main/java/org/saiku/service/util/export/excel/ExcelWorksheetBuilder.java
+++ saiku-core/saiku-service/src/main/java/org/saiku/service/util/export/excel/ExcelWorksheetBuilder.java
@@ -5,6 +5,8 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.saiku.olap.dto.SaikuDimensionSelection;
+import org.saiku.olap.dto.SaikuSelection;
 import org.saiku.olap.dto.resultset.AbstractBaseCell;
 import org.saiku.olap.dto.resultset.CellDataSet;
 import org.saiku.olap.dto.resultset.DataCell;
@@ -12,7 +14,10 @@ import org.saiku.service.util.exception.SaikuServiceException;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -32,6 +37,7 @@ public class ExcelWorksheetBuilder {
     private AbstractBaseCell[][] rowsetBody;
     private HSSFWorkbook excelWorkbook;
     private Sheet workbookSheet;
+    private String sheetName;
     private int topLeftCornerWidth;
     private int topLeftCornerHeight;
     private HSSFCellStyle basicCS;
@@ -39,17 +45,18 @@ public class ExcelWorksheetBuilder {
     private HSSFCellStyle dateCS;
     private HSSFCellStyle lighterHeaderCellCS;
     private HSSFCellStyle darkerHeaderCellCS;
+    private List<SaikuDimensionSelection> queryFilters;
 
-    public ExcelWorksheetBuilder(CellDataSet table, String sheetName) {
+    public ExcelWorksheetBuilder(CellDataSet table, List<SaikuDimensionSelection> filters, String sheetName) {
 
-        init(table, sheetName);
+        init(table, filters, sheetName);
     }
 
-    protected void init(CellDataSet table, String sheetName) {
+    protected void init(CellDataSet table, List<SaikuDimensionSelection> filters, String sheetName) {
 
+        queryFilters = filters;
         excelWorkbook = new HSSFWorkbook();
-        workbookSheet = excelWorkbook.createSheet(sheetName);
-
+        this.sheetName = sheetName;
         rowsetHeader = table.getCellSetHeaders();
         rowsetBody = table.getCellSetBody();
 
@@ -111,8 +118,10 @@ public class ExcelWorksheetBuilder {
 
     public byte[] build() throws SaikuServiceException {
 
-        int lastHeaderRow = buildExcelTableHeader();
+        int startRow = initExcelSheet();
+        int lastHeaderRow = buildExcelTableHeader(startRow);
         addExcelTableRows(lastHeaderRow);
+        finalizeExcelSheet(startRow);
 
         ByteArrayOutputStream bout = new ByteArrayOutputStream();
 
@@ -122,6 +131,77 @@ public class ExcelWorksheetBuilder {
             throw new SaikuServiceException("Error creating excel export for query", e);
         }
         return bout.toByteArray();
+    }
+
+    private void finalizeExcelSheet(int startRow) {
+
+        int headerWidth = rowsetHeader.length;
+
+        // Autosize columns
+        for (int i=0; i<rowsetBody[0].length; i++) {
+            workbookSheet.autoSizeColumn(i);
+        }
+
+        // Freeze the header columns
+        workbookSheet.createFreezePane( 0, startRow + headerWidth, 0, startRow + headerWidth );
+    }
+
+    private int initExcelSheet() {
+
+        initSummarySheet();
+        // Main Workbook Sheet
+        workbookSheet = excelWorkbook.createSheet(this.sheetName);
+
+        return 0;
+    }
+
+    private void initSummarySheet() {
+
+        // Main Workbook Sheet
+        Sheet summarySheet = excelWorkbook.createSheet("Summary page");
+
+        int row = 1;
+
+        Row sheetRow = summarySheet.createRow((short) row);
+        Cell cell = sheetRow.createCell(0);
+        String todayDate = (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")).format(new Date());
+        cell.setCellValue("Export date and time: " + todayDate);
+        summarySheet.addMergedRegion(new CellRangeAddress(1, 1, 0, 2));
+        row = row+2;
+
+        sheetRow = summarySheet.createRow((short) row);
+        cell = sheetRow.createCell(0);
+        cell.setCellValue("Dimension");
+        cell = sheetRow.createCell(1);
+        cell.setCellValue("Level");
+        cell = sheetRow.createCell(2);
+        cell.setCellValue("Filter Applied");
+        row++;
+
+        for (SaikuDimensionSelection item : queryFilters) {
+            for (SaikuSelection s : item.getSelections()) {
+                sheetRow = summarySheet.createRow((short) row);
+                cell = sheetRow.createCell(0);
+                cell.setCellValue(s.getDimensionUniqueName());
+                cell = sheetRow.createCell(1);
+                cell.setCellValue(s.getLevelUniqueName());
+                cell = sheetRow.createCell(2);
+                cell.setCellValue(s.getCaption());
+                row++;
+            }
+        }
+
+        row += 2;
+
+        sheetRow = summarySheet.createRow((short) row);
+        cell = sheetRow.createCell(0);
+        cell.setCellValue("Export made using Saiku OLAP client.");
+        summarySheet.addMergedRegion(new CellRangeAddress(row, row, 0, 10));
+
+        // Autosize columns for summary sheet
+        for (int i=0; i<5; i++) {
+            summarySheet.autoSizeColumn(i);
+        }
     }
 
     private void addExcelTableRows(int startingRow) {
@@ -153,7 +233,7 @@ public class ExcelWorksheetBuilder {
         }
     }
 
-    protected int buildExcelTableHeader() {
+    protected int buildExcelTableHeader(int startRow) {
 
         Row sheetRow = null;
         int x = 0;
@@ -161,35 +241,47 @@ public class ExcelWorksheetBuilder {
         int startSameFromPos = 0;
         int mergedCellsWidth = 0;
         boolean isLastHeaderRow = false;
-        String prevHeader = EMPTY_STRING;
+        boolean isLastColumn = false;
+        String nextHeader = EMPTY_STRING;
         String currentHeader = EMPTY_STRING;
         ArrayList<ExcelMergedRegionItemConfig> mergedItemsConfig = new ArrayList<ExcelMergedRegionItemConfig>();
 
         for (x = 0; x < rowsetHeader.length; x++) {
 
-            sheetRow = workbookSheet.createRow((short) x);
-            prevHeader = EMPTY_STRING;
+            sheetRow = workbookSheet.createRow((short) x + startRow);
+
+            nextHeader = EMPTY_STRING;
+            isLastColumn = false;
             startSameFromPos = 0;
             mergedCellsWidth = 0;
+
             if (x + 1 == rowsetHeader.length) isLastHeaderRow = true;
 
             for (y = 0; y < rowsetHeader[x].length; y++) {
                 currentHeader = rowsetHeader[x][y].getFormattedValue();
-                manageColumnHeaderDisplay(sheetRow, x, y, currentHeader);
-                if (!isLastHeaderRow) {
-                    if (currentHeader != null && (prevHeader.equals(EMPTY_STRING) || !prevHeader.equals(currentHeader))) {
-                        manageCellsMerge(y,
-                                         x,
-                                         mergedCellsWidth + 1,
-                                         (prevHeader.equals(EMPTY_STRING) ? y : startSameFromPos),
-                                         mergedItemsConfig);
-                        prevHeader = currentHeader;
-                        startSameFromPos = y;
-                        mergedCellsWidth = 0;
-                    } else if (currentHeader != null && prevHeader.equals(currentHeader)) {
-                        mergedCellsWidth++;
+                if (currentHeader != null) {
+                    if (rowsetHeader[x].length == y+1)
+                        isLastColumn = true;
+                    else
+                        nextHeader = rowsetHeader[x][y+1].getFormattedValue();
+
+                    manageColumnHeaderDisplay(sheetRow, x, y, currentHeader);
+
+                    if (!isLastHeaderRow) {
+                        if (!nextHeader.equals(currentHeader) || isLastColumn) {
+                            manageCellsMerge(y,
+                                             x + startRow,
+                                             mergedCellsWidth + 1,
+                                             startSameFromPos,
+                                             mergedItemsConfig);
+                            startSameFromPos = y+1;
+                            mergedCellsWidth = 0;
+                        } else if (nextHeader.equals(currentHeader)) {
+                            mergedCellsWidth++;
+                        }
                     }
-                }
+                } else
+                    startSameFromPos++;
         }
         // Manage the merge condition on exit from columns scan
         if (!isLastHeaderRow)
@@ -197,7 +289,7 @@ public class ExcelWorksheetBuilder {
         }
 
         if (topLeftCornerHeight > 0 && topLeftCornerWidth > 0) {
-            workbookSheet.addMergedRegion(new CellRangeAddress(0, topLeftCornerHeight - 1, 0, topLeftCornerWidth - 1));
+            workbookSheet.addMergedRegion(new CellRangeAddress(startRow, startRow + topLeftCornerHeight - 1, 0, topLeftCornerWidth - 1));
         }
 
         if (mergedItemsConfig.size()>0) {
@@ -207,7 +299,7 @@ public class ExcelWorksheetBuilder {
             }
         }
 
-        return x;
+        return x + startRow;
     }
 
     private void manageColumnHeaderDisplay(Row sheetRow, int x, int y, String currentHeader) {
