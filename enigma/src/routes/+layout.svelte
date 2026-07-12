291ab45e72351495d46f72da5fafdfff78a34370
Tom Barber <tom@spicule.co.uk>
2026-07-12 14:49:09 -0400

    feat(enigma): app shell + top bar

--- enigma/src/routes/+layout.svelte
+++ enigma/src/routes/+layout.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-
+	import '../app.css';
+	import TopBar from '$lib/components/TopBar.svelte';
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
+<TopBar />
 {@render children()}
